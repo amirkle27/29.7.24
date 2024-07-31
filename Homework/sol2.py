e2afc4dd5b43fc19117c5a10567a1ea9cd1ceab6
amirkle27 <amirkle@gmail.com>
2024-07-31 19:13:22 +0300

    Update sol2.py

--- Homework/sol2.py
+++ Homework/sol2.py
@@ -55,3 +55,35 @@ print(" 5 ".center(30, "!"))
 print()
 
 print(func_with_return.get_int ("what's your number?"))
+
+#5(e)#
+print(" 5(e) ".center(30, "!"))
+print()
+
+grades_list: list = []
+while True:
+    grade = (func_with_return.my_statistics("What's your grade? [-99 to Exit]"))
+    if not grade == -99:
+        grades_list.append(grade)
+    if grade == -99:
+        break
+print(grades_list)
+
+#6#
+print(" 6 ".center(30, "!"))
+print()
+
+"""If we don't use Return, there will be no output for the function.
+for example, for 5(e) the user will be asked again and again for the grade
+but the loop will not end, and the numbers will not be appended. No output back from the function.
+"""
+
+"""For a function with "Return None", we will either get the printout "None" (if originally asked to print the function's output)
+or there will be no response from the function, like not adding a Return at all.
+"""
+
+"""The similarity between "Return" and "Break" is that "Break" exits a loop if a condition is True,
+and "Return" exit the function if a condition is True. 
+But while "Break" exits the loop and actually breaks it, "Return" sends an output from the function
+"""
+
